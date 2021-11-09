ff89f1ea7784c3bc50bd84d4ac90d6d11879d49b
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-09 10:13:50 -0500

    Remove commented code

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,18 +3,6 @@ import Providers from "next-auth/providers";
 import { db } from "../../../firebase";
 import { FirebaseAdapter } from "@next-auth/firebase-adapter";
 
-// import NextAuth from "next-auth";
-// import GoogleProvider from "next-auth/providers/google";
-// import { FirebaseAdapter } from "@next-auth/firebase-adapter";
-// import { db } from "../../../firebase";
-
-// import firebase from "firebase/app";
-// import "firebase/firestore";
-
-// const firestore = (
-//   firebase.apps[0] ?? firebase.initializeApp(firebaseConfig)
-// ).firestore();
-
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
