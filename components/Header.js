8b89ff92e781634533ec03da2a86c6a6497973a3
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-12 09:45:31 -0500

    Implement item visibility based on session status

--- components/Header.js
+++ components/Header.js
@@ -5,9 +5,13 @@ import {
   PlusIcon,
   StarIcon,
 } from "@heroicons/react/solid";
-import { signIn } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
 
 const Header = () => {
+  const [session] = useSession();
+  const router = useRouter;
+
   return (
     <div className="sticky bg-[#040714] top-0 z-[1000] flex items-center px-10 h-[72px] md:px-12">
       <Image
@@ -16,39 +20,49 @@ const Header = () => {
         height={80}
         className="cursor-pointer"
       />
-
-      <div className="hidden ml-10 md:flex items-center space-x-6">
-        <a className="header-link group">
-          <HomeIcon className="h-4" />
-          <span className="span">Home</span>
-        </a>
-        <a className="header-link group">
-          <SearchIcon className="h-4" />
-          <span className="span">Search</span>
-        </a>
-        <a className="header-link group">
-          <PlusIcon className="h-4" />
-          <span className="span">Watchlist</span>
-        </a>
-        <a className="header-link group">
-          <StarIcon className="h-4" />
-          <span className="span">Originals</span>
-        </a>
-        <a className="header-link group">
-          <img src="/images/movie-icon.svg" alt="" className="h-5" />
-          <span className="span">Movies</span>
-        </a>
-        <a className="header-link group">
-          <img src="/images/series-icon.svg" alt="" className="h-5" />
-          <span className="span">Series</span>
-        </a>
-      </div>
-      <button
-        className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
-        onClick={signIn}
-      >
-        Login
-      </button>
+      {session && (
+        <div className="hidden ml-10 md:flex items-center space-x-6">
+          <a className="header-link group">
+            <HomeIcon className="h-4" />
+            <span className="span">Home</span>
+          </a>
+          <a className="header-link group">
+            <SearchIcon className="h-4" />
+            <span className="span">Search</span>
+          </a>
+          <a className="header-link group">
+            <PlusIcon className="h-4" />
+            <span className="span">Watchlist</span>
+          </a>
+          <a className="header-link group">
+            <StarIcon className="h-4" />
+            <span className="span">Originals</span>
+          </a>
+          <a className="header-link group">
+            <img src="/images/movie-icon.svg" alt="" className="h-5" />
+            <span className="span">Movies</span>
+          </a>
+          <a className="header-link group">
+            <img src="/images/series-icon.svg" alt="" className="h-5" />
+            <span className="span">Series</span>
+          </a>
+        </div>
+      )}
+      {!session ? (
+        <button
+          className="ml-auto uppercase border px-4 py-1.5 rounded font-medium tracking-wide hover:bg-white hover:text-black transition duration-200"
+          onClick={signIn}
+        >
+          Login
+        </button>
+      ) : (
+        <img
+          src={session.user.image}
+          alt=""
+          className="ml-auto h-12 w-12 rounded-full object-cover cursor-pointer"
+          onClick={signOut}
+        />
+      )}
     </div>
   );
 };
