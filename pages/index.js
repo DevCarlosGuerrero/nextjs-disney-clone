5f967604f3e283ad7e0d289829f58d1749bcb338
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-17 08:31:36 -0500

    Implement Fetch requests for movie and shows data

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,15 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import Slider from "../components/Slider";
 import Brands from "../components/Brands";
+import MoviesCollection from "../components/MoviesCollection";
+import ShowsCollection from "../components/ShowsCollection";
 
-export default function Home() {
+export default function Home({
+  popularMovies,
+  popularShows,
+  topRatedMovies,
+  topRatedShows,
+}) {
   const [session] = useSession();
   return (
     <div className="">
@@ -22,6 +29,8 @@ export default function Home() {
         <main className="relative min-h-screen after:bg-home after:bg-center after:bg-cover after:bg-no-repeat after:bg-fixed after:absolute after:inset-0 after:z-[-1]">
           <Slider />
           <Brands />
+          <MoviesCollection results={popularMovies} />
+          <ShowsCollection results={popularShows} />
         </main>
       )}
     </div>
@@ -30,10 +39,41 @@ export default function Home() {
 
 export async function getServerSideProps(context) {
   const session = await getSession(context);
+  const [
+    popularMoviesRes,
+    popularShowsRes,
+    topRatedMoviesRes,
+    topRatedShowsRes,
+  ] = await Promise.all([
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+    fetch(
+      `https://api.themoviedb.org/3/tv/popular?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+    fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+    fetch(
+      `https://api.themoviedb.org/3/tv/top_rated?api_key=${process.env.TMDB_API_KEY}&language=en-US&page=1`
+    ),
+  ]);
+
+  const [popularMovies, popularShows, topRatedMovies, topRatedShows] =
+    await Promise.all([
+      popularMoviesRes.json(),
+      popularShowsRes.json(),
+      topRatedMoviesRes.json(),
+      topRatedShowsRes.json(),
+    ]);
 
   return {
     props: {
       session,
+      popularMovies: popularMovies.results,
+      popularShows: popularShows.results,
+      topRatedMovies: topRatedMovies.results,
+      topRatedShows: topRatedShows.results,
     },
   };
 }
