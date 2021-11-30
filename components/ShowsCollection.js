7e3cfba6fd3c58cf378ee246cfbba10dd089f872
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-30 10:28:08 -0500

    Implement ShowsCollection component

--- components/ShowsCollection.js
+++ components/ShowsCollection.js
@@ -1,7 +1,14 @@
-const ShowsCollection = () => {
+import MovieThumbnail from "./MovieThumbnail";
+
+const ShowsCollection = ({ results, title }) => {
   return (
-    <div>
-      <h1>Shows Collection</h1>
+    <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
+      <h2 className="font-semibold">{title}</h2>
+      <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2">
+        {results.map((result) => (
+          <MovieThumbnail key={result.id} result={result} />
+        ))}
+      </div>
     </div>
   );
 };
--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,6 +1,38 @@
+import { getSession, useSession } from "next-auth/client";
+import Head from "next/head";
+
+// Components
+import Image from "next/image";
+import Header from "../../components/Header";
+import Hero from "../../components/Hero";
+
 const Movie = ({ result }) => {
+  const [session] = useSession();
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
+
   return (
     <div>
+      <Head>
+        <title>{result.title || result.original_name}</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      {!session ? (
+        <Hero />
+      ) : (
+        <section>
+          <div className="relative min-h-[calc(100vh-72px)]">
+            <Image
+              src={
+                `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+                `${BASE_URL}${result.poster_path}`
+              }
+              layout="fill"
+              objectFit="cover"
+            />
+          </div>
+        </section>
+      )}
       <h1>{result.title}</h1>
     </div>
   );
@@ -9,6 +41,7 @@ const Movie = ({ result }) => {
 export default Movie;
 
 export async function getServerSideProps(context) {
+  const session = await getSession(context);
   const { id } = context.query;
   const request = await fetch(
     `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}&language=en-US&append_to_response=videos`
@@ -16,6 +49,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
+      session,
       result: request,
     },
   };
