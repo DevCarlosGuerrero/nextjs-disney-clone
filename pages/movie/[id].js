32e351edd91d3e741c8a8475af1d34e8016cc63d
Carlos Guerrero <carlos.guerrero292@hotmail.com>
2021-11-30 23:48:52 -0500

    Add initial styles to page

--- pages/movie/[id].js
+++ pages/movie/[id].js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { getSession, useSession } from "next-auth/client";
 import Head from "next/head";
 
@@ -6,9 +7,13 @@ import Image from "next/image";
 import Header from "../../components/Header";
 import Hero from "../../components/Hero";
 
+// Icons
+import { PlusIcon } from "@heroicons/react/solid";
+
 const Movie = ({ result }) => {
   const [session] = useSession();
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
+  const [showPlayer, setShowPlayer] = useState(false);
 
   return (
     <div>
@@ -20,7 +25,7 @@ const Movie = ({ result }) => {
       {!session ? (
         <Hero />
       ) : (
-        <section>
+        <section className="relative z-50">
           <div className="relative min-h-[calc(100vh-72px)]">
             <Image
               src={
@@ -31,9 +36,50 @@ const Movie = ({ result }) => {
               objectFit="cover"
             />
           </div>
+          <div className="absolute inset-y-28 md:inset-y-auto md:bottom-10 inset-x-4 md:inset-x-12 space-y-6 z-50">
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
+              {result.title || result.original_name}
+            </h1>
+            <div className="flex items-center space-x-3 md:space-x-5">
+              <button className="flex items-center justify-center py-1.5 px-12 rounded text-xs md:text-base bg-[#f9f9f9] text-black hover:bg-[#c6c6c6]">
+                <img
+                  src="/images/play-icon-black.svg"
+                  alt="play button"
+                  className="h-6 md:h-8"
+                />
+                <span className="uppercase font-medium tracking-wide">
+                  Play
+                </span>
+              </button>
+              <button
+                className="flex items-center justify-center py-1.5 px-12 rounded text-xs md:text-base bg-black/30 text-[#f9f9f9] border border-[#f9f9f9] hover:bg-[#c6c6c6]"
+                onClick={() => setShowPlayer(true)}
+              >
+                <img
+                  src="/images/play-icon-white.svg"
+                  alt="trailer play button"
+                  className="h-6 md:h-8"
+                />
+                <span className="uppercase font-medium tracking-wide">
+                  Trailer
+                </span>
+              </button>
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <PlusIcon className="h-6" />
+              </div>
+              <div className="rounded-full border-2 border-white flex items-center justify-center w-11 h-11 cursor-pointer bg-black/60">
+                <img src="/images/group-icon.svg" alt="group" />
+              </div>
+            </div>
+
+            <p className="text-xs md:text-sm">
+              {result.release_date || result.first_air_date} •{" "}
+              {Math.floor(result.runtime / 60)}h {result.runtime % 60}m •{" "}
+              {result.genres.map((genre) => genre.name + " ")}{" "}
+            </p>
+          </div>
         </section>
       )}
-      <h1>{result.title}</h1>
     </div>
   );
 };
